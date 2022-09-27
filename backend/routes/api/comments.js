e375a38255e027665fdd3b98ba6196a203690244
Reptar007 <sebe4106@gmail.com>
2022-09-26 22:39:04 -0400

    working DELETE comment by commentId

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -8,6 +8,13 @@ const router = express.Router();
 router.put('/:commentId', requireAuth, async(req, res, next) =>{
     const comment = await Comment.findByPk(req.params.commentId)
 
+    if(!comment) {
+        res.json({                    
+            "message": "Comment couldn't be found",
+            "statusCode": 404
+        })
+    }
+
     if(comment.userId === req.user.id) {
         const { body } = req.body
         const updatedcomment = await comment.update({
@@ -19,6 +26,29 @@ router.put('/:commentId', requireAuth, async(req, res, next) =>{
       err.status = 403;
       next(err);
     }
+})
+
+router.delete('/:commentId', requireAuth, async(req,res,next) =>{
+     const comment = await Comment.findByPk(req.params.commentId);
+
+     if (!comment) {
+       res.json({
+         message: "Comment couldn't be found",
+         statusCode: 404,
+       });
+     }
+
+     if (comment.userId === req.user.id) {
+       await comment.destroy()
+       res.json({
+         message: "Successfully deleted",
+         statusCode: 200,
+       });
+     } else {
+       const err = new Error("Forbidden");
+       err.status = 403;
+       next(err);
+     }
 })
 
 module.exports = router;
