cd273f306231abed18233a0c53ffc9a0bc64e9e6
Reptar007 <sebe4106@gmail.com>
2022-10-13 17:37:59 -0400

    sing up

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -38,17 +38,19 @@ export const login = (user) => async (dispatch) => {
 };
 
 export const signup = (user) => async (dispatch) => {
-  const { username, email, password } = user;
+  const { firstName, lastName, username, email, password } = user;
   const response = await csrfFetch("/api/users", {
     method: "POST",
     body: JSON.stringify({
-      username,
-      email,
-      password,
+        firstName,
+        lastName,
+        username,
+        email,
+        password,
     }),
   });
   const data = await response.json();
-  dispatch(setUser(data.user));
+  dispatch(setUser(data));
   return response;
 };
 
