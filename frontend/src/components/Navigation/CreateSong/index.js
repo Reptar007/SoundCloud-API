5155a7e2489abd848e92a2f34233f0d012e12fda
Reptar007 <sebe4106@gmail.com>
2022-10-20 16:47:04 -0400

    finished error handling

--- frontend/src/components/Navigation/CreateSong/index.js
+++ frontend/src/components/Navigation/CreateSong/index.js
@@ -13,26 +13,27 @@ function CreateSongForm({ user }) {
     const history = useHistory()
 
     const [title, setTitle] = useState('')
-    const [description, setDescription] = useState()
+    const [description, setDescription] = useState('')
     const [url, setUrl] = useState('')
     const [imageUrl, setImageUrl] = useState()
     const [albumId, setAlbumId] = useState(null)
-    const [validateErrors, setValidateErrors] = useState([])
+    const [validateErrors, setValidateErrors] = useState({})
     const [hasSubmitted, setHasSubmitted] = useState(false)
 
     useEffect(() => {
-        const errors = []
-        if(title.length === 0) errors.push("Song title is required");
-        if(url.length === 0) errors.push("Audio is required");
+        const errors = {}
+        if(title.length === 0) errors.title = "Song title is required"
+        if(url.length === 0) errors.url = "Audio is required"
+        if(description.length > 250 ) errors.description = "Description can't be longer than 250 character"
         setValidateErrors(errors)
-    },[title, url])
+    },[title, url,description])
     
     const handleSubmit = async(e) => {
         
         e.preventDefault()
         setHasSubmitted(true)
 
-        if(validateErrors.length > 0) return
+        if(Object.keys(validateErrors).length > 0) return
 
         const payload = {
             title,
@@ -42,10 +43,8 @@ function CreateSongForm({ user }) {
             albumId,
         }
 
-
-
         let createdSong = await dispatch(createASongThunkCreator(payload))
-        if(createdSong && validateErrors.length === 0) {
+        if(createdSong && Object.keys(validateErrors).length === 0) {
             history.push(`/${user.id}`)
         }
 
@@ -54,7 +53,7 @@ function CreateSongForm({ user }) {
         setUrl('')
         setImageUrl('')
         setAlbumId('')
-        setValidateErrors('')
+        setValidateErrors({})
     }
 
     return (
@@ -62,45 +61,67 @@ function CreateSongForm({ user }) {
         <div className="formContainer">
           <form onSubmit={handleSubmit}>
             <h1>Ready to create your hatchling?</h1>
-            <ul className='errors'>
-            {hasSubmitted && validateErrors.length > 0 && (
-               validateErrors.map((error, idx) => (
-                <li key={idx}>{error}</li>
-                )))}
-                </ul>
-              <input
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                placeholder="Title"
-              />
-              <input
-                type="text"
-                value={url}
-                onChange={(e) => setUrl(e.target.value)}
-                placeholder='Audio URL'
-              />
-              <input
-                type="text"
-                value={imageUrl}
-                onChange={(e) => setImageUrl(e.target.value)}
-                placeholder='Image URL'
-              />
-              <input
-                type="number"
-                value={albumId}
-                onChange={(e) => setAlbumId(e.target.value)}
-                placeholder='Album ID if you have one'
-              />
-              <input
-                type="text"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                placeholder='Description'
-              />
-            <button type="submit" >
-              Create Song
-            </button>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Title"
+            />
+            {hasSubmitted && validateErrors.title && (
+              <li className="errors">
+                <img
+                  className="errorDuck"
+                  src="https://i.imgur.com/7OuSWd1.png"
+                  alt=""
+                />{" "}
+                {validateErrors.title}
+              </li>
+            )}
+            <input
+              type="text"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+              placeholder="Audio URL"
+            />
+            {hasSubmitted && validateErrors.url && (
+              <li className="errors">
+                <img
+                  className="errorDuck"
+                  src="https://i.imgur.com/7OuSWd1.png"
+                  alt=""
+                />{" "}
+                {validateErrors.url}
+              </li>
+            )}
+            <input
+              type="text"
+              value={imageUrl}
+              onChange={(e) => setImageUrl(e.target.value)}
+              placeholder="Image URL"
+            />
+            <input
+              type="number"
+              value={albumId}
+              onChange={(e) => setAlbumId(e.target.value)}
+              placeholder="Album ID if you have one"
+            />
+            <input
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="Description"
+            />
+            {hasSubmitted && validateErrors.description && (
+              <li className="errors">
+                <img
+                  className="errorDuck"
+                  src="https://i.imgur.com/7OuSWd1.png"
+                  alt=""
+                />{" "}
+                {validateErrors.description}
+              </li>
+            )}
+            <button type="submit">Create Song</button>
           </form>
         </div>
       </div>
--- frontend/src/components/UpdateSongModal/UpdateSongForm.js
+++ frontend/src/components/UpdateSongModal/UpdateSongForm.js
@@ -13,17 +13,23 @@ function UpdateSongForm({ song, setShowModal }) {
   const [url, setUrl] = useState(song.url || '');
   const [imageUrl, setImageUrl] = useState(song.imageUrl || '');
   const [albumId, setAlbumId] = useState(song.albumId || null);
-  const [validateErrors, setValidateErrors] = useState([]);
+  const [hasSubmitted, setHasSubmitted] = useState(false)
+  const [validateErrors, setValidateErrors] = useState({});
 
   useEffect(() => {
-    const errors = [];
-    if (title.length === 0) errors.push("Song title is required");
-    if (url.length === 0) errors.push("Audio is required");
+    const errors = {};
+    if (title.length === 0) errors.title = "Song title is required";
+    if (url.length === 0) errors.url = "Audio is required";
+    if (description.length > 250)
+      errors.description = "Description can't be longer than 250 character";
     setValidateErrors(errors);
-  }, [title, url]);
+  }, [title, url, description]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setHasSubmitted(true)
+
+    if (Object.keys(validateErrors).length > 0) return;
 
     const payload = {
       title,
@@ -39,46 +45,77 @@ function UpdateSongForm({ song, setShowModal }) {
       setShowModal(false)
     }
 
+    setTitle("");
+    setDescription("");
+    setUrl("");
+    setImageUrl("");
+    setAlbumId("");
+    setValidateErrors({});
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <ul>
-        {validateErrors.map((error, idx) => (
-          <li key={idx}>{error}</li>
-        ))}
-      </ul>
       <h1>Update your little hatchling: </h1>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder='Title'
-        />
-        <input
-          type="text"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-          placeholder='Audio Url'
-        />
-        <input
-          type="text"
-          value={imageUrl}
-          onChange={(e) => setImageUrl(e.target.value)}
-          placeholder='Image Url'
-        />  
-        <input
-          type="number"
-          value={albumId}
-          onChange={(e) => setAlbumId(e.target.value)}
-          placeholder='Album ID'
-        /> 
-        <input
-          type="text"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          placeholder='Description'
-        />
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Title"
+      />
+      {hasSubmitted && validateErrors.title && (
+        <li className="errors">
+          <img
+            className="errorDuck"
+            src="https://i.imgur.com/7OuSWd1.png"
+            alt=""
+          />{" "}
+          {validateErrors.title}
+        </li>
+      )}
+      <input
+        type="text"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+        placeholder="Audio Url"
+      />
+      {hasSubmitted && validateErrors.url && (
+        <li className="errors">
+          <img
+            className="errorDuck"
+            src="https://i.imgur.com/7OuSWd1.png"
+            alt=""
+          />{" "}
+          {validateErrors.url}
+        </li>
+      )}
+      <input
+        type="text"
+        value={imageUrl}
+        onChange={(e) => setImageUrl(e.target.value)}
+        placeholder="Image Url"
+      />
+      <input
+        type="number"
+        value={albumId}
+        onChange={(e) => setAlbumId(e.target.value)}
+        placeholder="Album ID"
+      />
+      <input
+        type="text"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="Description"
+      />
+      {hasSubmitted && validateErrors.description && (
+        <li className="errors">
+          <img
+            className="errorDuck"
+            src="https://i.imgur.com/7OuSWd1.png"
+            alt=""
+          />{" "}
+          {validateErrors.description}
+        </li>
+      )}
       <button type="submit">Update Song</button>
     </form>
   );
