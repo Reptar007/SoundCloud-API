bff28430ada3c21b39c23dacc5dbe9eb2da332a8
Reptar007 <sebe4106@gmail.com>
2022-10-21 18:44:07 -0400

    extra error validators

--- frontend/src/components/Navigation/CreateSong/index.js
+++ frontend/src/components/Navigation/CreateSong/index.js
@@ -23,12 +23,13 @@ function CreateSongForm({ user }) {
     useEffect(() => {
         const errors = {}
         if(title.length === 0) errors.title = "Song title is required"
-        if(title.length > 100) errors.title = "Song title can't be longer than 100 characters"
+        if(title.length > 50) errors.title = "Song title can't be longer than 100 characters"
         if(url.length === 0) errors.url = "Audio is required"
-        // if(albumId < 0) errors.albumId = "Album ID can't be negative"
-        if(description.length > 250 ) errors.description = "Description can't be longer than 250 characters"
+        if(!/^https?:\/\/.+\.(wav|mp3|aac|flac)$/.test(url)) errors.url = 'Audio must be a real audio file'
+        if (!/^https?:\/\/.+\.(jpg|jpeg|png|JPG|JPEG|PNG)$/.test(imageUrl)) errors.imageUrl ='Must be a real image'
+        if (description.length > 250) errors.description = "Description can't be longer than 250 characters";
         setValidateErrors(errors)
-    },[title, url,description])
+    },[title, url,description,imageUrl])
     
     const handleSubmit = async(e) => {
         
@@ -54,7 +55,6 @@ function CreateSongForm({ user }) {
         setDescription('')
         setUrl('')
         setImageUrl('')
-        // setAlbumId('')
         setValidateErrors({})
     }
 
@@ -101,22 +101,16 @@ function CreateSongForm({ user }) {
               onChange={(e) => setImageUrl(e.target.value)}
               placeholder="Image URL"
             />
-            {/* <input
-              type="number"
-              value={albumId}
-              onChange={(e) => setAlbumId(e.target.value)}
-              placeholder="Album ID if you have one"
-            />
-            {hasSubmitted && validateErrors.albumId && (
+            {hasSubmitted && validateErrors.imageUrl && (
               <li className="errors">
                 <img
                   className="errorDuck"
                   src="https://i.imgur.com/7OuSWd1.png"
                   alt=""
                 />{" "}
-                {validateErrors.albumId}
+                {validateErrors.imageUrl}
               </li>
-            )} */}
+            )}
             <input
               type="text"
               value={description}
--- frontend/src/components/UpdateSongModal/UpdateSongForm.js
+++ frontend/src/components/UpdateSongModal/UpdateSongForm.js
@@ -19,14 +19,18 @@ function UpdateSongForm({ song, setShowModal }) {
   useEffect(() => {
     const errors = {};
     if (title.length === 0) errors.title = "Song title is required";
-    if (title.length > 250)
+    if (title.length > 50)
       errors.title = "Song title can't be longer than 250 characters";
     if (url.length === 0) errors.url = "Audio is required";
+     if (!/^https?:\/\/.+\.(wav|mp3|aac|flac)$/.test(url))
+       errors.url = "Audio must be a real audio file";
+     if (!/^https?:\/\/.+\.(jpg|jpeg|png|JPG|JPEG|PNG)$/.test(imageUrl))
+       errors.imageUrl = "Must be a real image";
     if (albumId < 0) errors.albumId = "Album ID can't be negative";
     if (description.length > 250)
       errors.description = "Description can't be longer than 250 characters";
     setValidateErrors(errors);
-  }, [title, url, description, albumId]);
+  }, [title, url, description, albumId, imageUrl]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
